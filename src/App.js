43470876af4aaab55afaa62536508eb7a3e97d72
alams3510 <alams3510@gmail.com>
2023-10-04 12:45:55 +0530

    corrected

--- src/App.js
+++ src/App.js
@@ -3,29 +3,20 @@ import "./App.css";
 import LandingpPage from "./pages/LandingpPage";
 import MainPage from "./pages/MainPage";
 import { Route, Routes } from "react-router-dom";
-import tiktik from "./sounds/clock-ticking.mp3";
 
 function App() {
   const [username, setUserName] = useState("");
-  const titiksound = new Audio(tiktik);
   return (
     <div className="main">
       <Routes>
         <Route
           path="/"
           element={
-            <LandingpPage
-              titiksound={titiksound}
-              setUserName={setUserName}
-              username={username}
-            />
+            <LandingpPage setUserName={setUserName} username={username} />
           }
         />
 
-        <Route
-          path="/main"
-          element={<MainPage titiksound={titiksound} username={username} />}
-        />
+        <Route path="/main" element={<MainPage username={username} />} />
       </Routes>
     </div>
   );
